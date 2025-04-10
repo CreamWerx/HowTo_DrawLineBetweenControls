d308d8a22344e4b9de130512788fb7b3a85754bd
Susan Dwyer <suzycream999@googlemail.com>
2025-04-10 21:12:15 +0100

    Add project files.

---
+++ HowTo_DrawLineBetweenControls/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HowTo_DrawLineBetweenControls;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+
+    private Button Target { get; set; }
+    public MainWindow()
+    {
+        InitializeComponent();
+        Target = TA1;
+    }
+
+    private void B_Click(object sender, RoutedEventArgs e)
+    {
+        var btn = sender as Button;
+        if (btn is null) return;
+        DrawLineToTarget(btn, e);
+    }
+
+    private void DrawLineToTarget(Button source, RoutedEventArgs e)
+    {
+        var pos1 = source.TranslatePoint(new Point(), TheGrid);
+        pos1.X += source.Width / 2;
+        pos1.Y += source.Height / 2;
+        var pos2 = Target.TranslatePoint(new Point(), TheGrid);
+        pos2.X += Target.Width / 2;
+        pos2.Y += Target.Height / 2;
+
+        string pathString = $"M {pos1.X},{pos1.Y} L {pos2.X},{pos2.Y}";
+        Path line = new();
+        line.Stroke = Brushes.Black;
+        line.Data = Geometry.Parse(pathString);
+        TheGridParent.Children.Add(line);
+    }
+}
